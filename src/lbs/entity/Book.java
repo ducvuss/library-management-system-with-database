564a9c277b071b64ce405238f862dab957248680
Duc Vu <duc.vu@smoothstack.com>
2020-04-23 19:34:31 -0500

    added book class

--- src/lbs/entity/Book.java
+++ src/lbs/entity/Book.java
@@ -8,5 +8,40 @@ package lbs.entity;
  *
  */
 public class Book {
+	private Integer bookId;
+	private String title;
+	private Integer publisherId;
+	private Publisher publisher;
+	
+	public Integer getBookId() {
+		return bookId;
+	}
+	
+	public void setBookId(Integer bookId) {
+		this.bookId = bookId;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public void setTitle(String title) {
+		this.title = title;
+	}
 
+	public Integer getPublisherId() {
+		return publisherId;
+	}
+
+	public void setPublisherId(Integer publisherId) {
+		this.publisherId = publisherId;
+	}
+
+	public Publisher getPublisher() {
+		return publisher;
+	}
+
+	public void setPublisher(Publisher publisher) {
+		this.publisher = publisher;
+	}
 }
