2aa26b53fa67fe7138a35c4e235928da0e2178fa
Duc Vu <duc.vu@smoothstack.com>
2020-04-24 07:19:46 -0500

    added query builder for insert statement

--- src/lms/utils/Table.java
+++ src/lms/utils/Table.java
@@ -3,6 +3,7 @@
  */
 package lms.utils;
 
+import java.util.function.BinaryOperator;
 import java.util.stream.Stream;
 
 /**
@@ -21,12 +22,33 @@ public class Table {
 	}
 
 	public String select(String... columnNames) {
-		return "SELECT " + Stream.of(columnNames).reduce("*", (str1, str2) -> {
-			if (str1.equals("*")) {
+		String columns = columnNames.length > 0 ? appendColumnName(columnNames) : "*";
+		return String.format("SELECT %s FROM %s", columns, tableName);
+	}
+
+	public String insert(String... columnNames) {
+		String columns = columnNames.length > 0 ? String.format(" (%s)", appendColumnName(columnNames)) : "";
+		return String.format("INSERT INTO %s%s VALUES (?)", tableName, columns);
+	}
+
+	/**
+	 * @param columnNames
+	 * @return
+	 */
+	private String appendColumnName(String[] columnNames) {
+		return Stream.of(columnNames).reduce("", writeColumnNames());
+	}
+
+	/**
+	 * @return
+	 */
+	private BinaryOperator<String> writeColumnNames() {
+		return (str1, str2) -> {
+			if (str1.isEmpty()) {
 				return str2;
 			}
 			return str1 + ", " + str2;
-		}) + " FROM " + tableName;
+		};
 	}
 
 }
--- src/lms/utils/test/TableTest.java
+++ src/lms/utils/test/TableTest.java
@@ -22,4 +22,10 @@ public class TableTest {
 				new Table("tbl_author").select("authorId, authorName"));
 	}
 
+	@Test
+	public void testInsert() {
+		assertEquals("INSERT INTO tbl_author VALUES (?)", new Table("tbl_author").insert());
+		assertEquals("INSERT INTO tbl_author (authorName) VALUES (?)", new Table("tbl_author").insert("authorName"));
+	}
+
 }
