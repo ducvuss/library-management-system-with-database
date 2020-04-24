750e739df3ac7f0747987652cc2356e09ca23ac9
Duc Vu <duc.vu@smoothstack.com>
2020-04-23 19:28:06 -0500

    implemented author class

--- src/lbs/entity/Author.java
+++ src/lbs/entity/Author.java
@@ -8,5 +8,17 @@ package lbs.entity;
  *
  */
 public class Author {
+	private String authorName;
 
+	public Author(String authorName) {
+		this.setAuthorName(authorName);
+	}
+
+	public String getAuthorName() {
+		return authorName;
+	}
+
+	public void setAuthorName(String authorName) {
+		this.authorName = authorName;
+	}
 }
