78588f3a9565664cac248fda6111216b18817461
Duc Vu <duc.vu@smoothstack.com>
2020-04-27 07:21:31 -0500

    Update BookLoansDAO.java

--- src/lms/dao/BookLoansDAO.java
+++ src/lms/dao/BookLoansDAO.java
@@ -31,12 +31,12 @@ public class BookLoansDAO extends BaseDAO<BookLoan> {
 		// TODO Auto-generated method stub
 		return null;
 	}
-	
+
 	public BookLoan get(Integer bookId, Integer branchId, Integer cardNo) throws SQLException {
-		List<BookLoan> bookLoans = extractData(read(new Table(tableName).select() + " WHERE bookId=? and branchId=? and cardNo=? and dateIn is null;", new Object[] {
-				bookId, branchId, cardNo
-		}));
-		
+		List<BookLoan> bookLoans = extractData(
+				read(new Table(tableName).select() + " WHERE bookId=? and branchId=? and cardNo=? and dateIn is null;",
+						new Object[] { bookId, branchId, cardNo }));
+
 		if (bookLoans.size() > 0) {
 			return bookLoans.get(0);
 		}
@@ -57,43 +57,55 @@ public class BookLoansDAO extends BaseDAO<BookLoan> {
 
 	}
 
+	public void update(BookLoan loan) throws SQLException {
+		save("UPDATE tbl_book_loans set dueDate=?, dateIn=? WHERE bookId=? and branchId=? and cardNo=? and dateOut=?",
+				new Object[] { loan.getDateIn(), loan.getDueDate(), loan.getBookId(), loan.getBranchId(),
+						loan.getCardNo(), loan.getDateOut() });
+	}
+
 	@Override
 	public void delete(Integer id) throws SQLException {
-		// TODO Auto-generated method stub
+	}
 
+	public void remove(BookLoan loan) throws SQLException {
+		save("DELETE FROM tbl_book_loans WHERE bookId=? and branchId=? and cardNo=? and dateOut=?",
+				new Object[] { loan.getBookId(), loan.getBranchId(), loan.getCardNo(), loan.getDateOut() });
 	}
 
 	@Override
 	public List<BookLoan> extractData(ResultSet results) throws SQLException {
 
 		List<BookLoan> bookLoans = new ArrayList<>();
-		
+
 		while (results.next()) {
-			
+
 			BookLoan bookLoan = new BookLoan();
 			bookLoan.setBranchId(results.getInt("branchId"));
 			bookLoan.setBookId(results.getInt("bookId"));
 			bookLoan.setCardNo(results.getInt("cardNo"));
 			if (results.getTimestamp("dateIn") != null) {
-				bookLoan.setDateIn(LocalDateTime.ofInstant(results.getTimestamp("dateIn").toInstant(), ZoneId.systemDefault()));
+				bookLoan.setDateIn(
+						LocalDateTime.ofInstant(results.getTimestamp("dateIn").toInstant(), ZoneId.systemDefault()));
 			}
 			if (results.getTimestamp("dateOut") != null) {
-				bookLoan.setDateOut(LocalDateTime.ofInstant(results.getTimestamp("dateOut").toInstant(), ZoneId.systemDefault()));
+				bookLoan.setDateOut(
+						LocalDateTime.ofInstant(results.getTimestamp("dateOut").toInstant(), ZoneId.systemDefault()));
 			}
 			if (results.getTimestamp("dueDate") != null) {
-				bookLoan.setDueDate(LocalDateTime.ofInstant(results.getTimestamp("dueDate").toInstant(), ZoneId.systemDefault()));
+				bookLoan.setDueDate(
+						LocalDateTime.ofInstant(results.getTimestamp("dueDate").toInstant(), ZoneId.systemDefault()));
 			}
-			
+
 			bookLoans.add(bookLoan);
 		}
-		
+
 		return bookLoans;
 	}
 
 	public void put(BookLoan loan) throws SQLException {
 		LocalDateTime now = LocalDateTime.now();
 		save("UPDATE tbl_book_loans SET dateIn=? WHERE bookId=? and branchId=? and cardNo=? and dateIn is null",
-				new Object[] { now, loan.getBookId(), loan.getBranchId(), loan.getCardNo()});
+				new Object[] { now, loan.getBookId(), loan.getBranchId(), loan.getCardNo() });
 	}
 
 }
